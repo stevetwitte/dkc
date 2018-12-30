d1a6e1fe69a1d87843e191bcd59367e06bac0e66
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-12-30 15:59:18 -0500

    Adds load method to config singleton with tests

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'dkc/config'
 
 RSpec.describe Dkc::Config do
@@ -6,9 +7,48 @@ RSpec.describe Dkc::Config do
     expect(Dkc::Config.instance).to be_truthy
   end
 
-  context "load" do
-    it "has an instance method" do
-      expect(Dkc::Config.instance).to be_truthy
+  context "a config file exists" do
+    before :context do
+      config_file = File.new(CONFIG_FILE_NAME, "w+")
+      config_data = { usesCompose: true }
+      config_file.write(config_data.to_json)
+      config_file.close
+    end
+
+    after :context do
+      File.delete(CONFIG_FILE_NAME)
+    end
+
+    it "returns true when calling load" do
+      expect(Dkc::Config.instance.load).to eql(true)
+    end
+
+    it "returns values from config object" do
+      expect(Dkc::Config.instance.getValue('usesCompose')).to eql(true)
     end
   end
+
+  context "a config does not exists" do
+    it "returns false when calling load" do
+      expect(Dkc::Config.instance.load).to eql(false)
+    end
+  end
+
+  context "a config file exists but is invalid" do
+    before :context do
+      config_file = File.new(CONFIG_FILE_NAME, "w+")
+      config_data = ""
+      config_file.write(config_data.to_json)
+      config_file.close
+    end
+
+    after :context do
+      File.delete(CONFIG_FILE_NAME)
+    end
+
+    it "returns false when calling load" do
+      expect(Dkc::Config.instance.load).to eql(false)
+    end
+  end
+
 end
