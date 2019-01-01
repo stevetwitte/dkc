3467a53741e96131365cb37fd6abda0f91e064c1
Stephen Taylor Witte <stevetwitte@gmail.com>
2019-01-01 11:54:58 -0500

    fixes test

--- spec/dkc_spec.rb
+++ spec/dkc_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Dkc do
 
   it "runs the start command" do
     content = capture(:stdout) { Dkc::Main.start }
-    expect(content.strip).to eq("Commands:\n  rspec bash            # Start Bash on main container\n  rspec help [COMMAND]  # Describe available commands or one specific command\n  rspec up              # Provision containers")
+    expect(content.strip).to eq("Commands:\n  rspec bash            # Start Bash on main container\n  \
+rspec down            # Stopping and removing containers\n  \
+rspec help [COMMAND]  # Describe available commands or one specific command\n  \
+rspec start           # Start containers\n  \
+rspec stop            # Stop containers\n  \
+rspec up              # Provision containers")
   end
 end
